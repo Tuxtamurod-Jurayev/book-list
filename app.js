78d71c51e282f606d750fce14db3b1255f79af01
Tuxtamurod <83809344+Tuxtamurod-Jurayev@users.noreply.github.com>
2022-10-12 08:51:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    const title = document.querySelector('#title'),
+        author = document.querySelector('#author'),
+        year = document.querySelector('#year'),
+        btn  =  document.querySelector('.btn'),
+        book = document.querySelector('#book-list')
+
+        
+
+})
